082f566be974361dce54416be41b4f3626e90d71
aaditya6106 <aaditya6106@gmail.com>
2025-08-26 16:40:00 +0530

    Create 10.py

---
+++ aaditya_9/Day 1/10.py
@@ -0,0 +1,5 @@
+#program to print college name in uppercase and lowercase
+
+clg_name = input("\nEnter your college name: ")
+print("\nYour college name is",clg_name.upper(),"in uppercase.")
+print("Your college name is",clg_name.lower(),"in lowercase.\n")
